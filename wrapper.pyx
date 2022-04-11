0b122ecdf53b112e2d47c79499943c27c2b31928
kutakieu <kutakieu22@gmail.com>
2022-04-12 02:16:31 +0900

    somehow changed package name to gpurenderer

--- wrapper.pyx
+++ wrapper.pyx
@@ -3,20 +3,20 @@ cimport numpy as np
 
 assert sizeof(int) == sizeof(np.int32_t)
 
-cdef extern from "src/manager.hh":
-    cdef cppclass C_GPUAdder "GPUAdder":
-        C_GPUAdder(np.int32_t*, int)
+cdef extern from "csrc/manager.hh":
+    cdef cppclass C_GPURenderer "GPURenderer":
+        C_GPURenderer(np.int32_t*, int)
         void increment()
         void retreive()
         void retreive_to(np.int32_t*, int)
 
-cdef class GPUAdder:
-    cdef C_GPUAdder* g
+cdef class GPURenderer:
+    cdef C_GPURenderer* g
     cdef int dim1
 
     def __cinit__(self, np.ndarray[ndim=1, dtype=np.int32_t] arr):
         self.dim1 = len(arr)
-        self.g = new C_GPUAdder(&arr[0], self.dim1)
+        self.g = new C_GPURenderer(&arr[0], self.dim1)
 
     def increment(self):
         self.g.increment()
