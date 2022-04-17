bdf166c35139ac3863072460f7bea94fcfe481f4
kutakieu <kutakieu22@gmail.com>
2022-04-18 00:07:01 +0900

    clean runners

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
-import gpurenderer
+import cudapkg.gpurenderer
 import numpy as np
-import numpy.testing as npt
+from PIL import Image
 
+canvas_width = 300
+canvas_height = 300
+arr = np.zeros([canvas_height, canvas_width, 3], dtype=np.int32)
+adder = cudapkg.gpurenderer.GPURenderer(arr.reshape(-1), canvas_height, canvas_width)
+adder.render()
 
-def test():
-    canvas_width = 300
-    canvas_height = 300
-    arr = np.zeros([canvas_height, canvas_width, 3], dtype=np.int32)
-    adder = gpurenderer.GPURenderer(arr.reshape(-1), canvas_height, canvas_width)
-    adder.render()
-
-    print(arr)
-
-
-test()
+Image.fromarray(arr.astype(np.uint8)).save("test.jpg")
