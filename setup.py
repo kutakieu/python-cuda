43c365d1f3ed7737210937212cc2ca1232e7138b
kutakieu <kutakieu22@gmail.com>
2022-04-18 00:15:55 +0900

    resolve conflict

--- setup.py
+++ setup.py
@@ -28,25 +28,31 @@ def locate_cuda():
     """
 
     # First check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
+    if "CUDAHOME" in os.environ:
+        home = os.environ["CUDAHOME"]
+        nvcc = pjoin(home, "bin", "nvcc")
     else:
         # Otherwise, search the PATH for NVCC
-        nvcc = find_in_path('nvcc', os.environ['PATH'])
+        nvcc = find_in_path("nvcc", os.environ["PATH"])
         if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be '
-                'located in your $PATH. Either add it to your path, '
-                'or set $CUDAHOME')
+            raise EnvironmentError(
+                "The nvcc binary could not be "
+                "located in your $PATH. Either add it to your path, "
+                "or set $CUDAHOME"
+            )
         home = os.path.dirname(os.path.dirname(nvcc))
 
-    cudaconfig = {'home': home, 'nvcc': nvcc,
-                  'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
+    cudaconfig = {
+        "home": home,
+        "nvcc": nvcc,
+        "include": pjoin(home, "include"),
+        "lib64": pjoin(home, "lib64"),
+    }
     for k, v in iter(cudaconfig.items()):
         if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not be '
-                                   'located in %s' % (k, v))
+            raise EnvironmentError(
+                "The CUDA %s path could not be " "located in %s" % (k, v)
+            )
 
     return cudaconfig
 
@@ -62,7 +68,7 @@ def customize_compiler_for_nvcc(self):
     """
 
     # Tell the compiler it can processes .cu
-    self.src_extensions.append('.cu')
+    self.src_extensions.append(".cu")
 
     # Save references to the default compiler_so and _comple methods
     default_compiler_so = self.compiler_so
@@ -72,14 +78,14 @@ def customize_compiler_for_nvcc(self):
     # object but distutils doesn't have the ability to change compilers
     # based on source extension: we add it.
     def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-        if os.path.splitext(src)[1] == '.cu':
+        if os.path.splitext(src)[1] == ".cu":
             # use the cuda for .cu files
-            self.set_executable('compiler_so', CUDA['nvcc'])
+            self.set_executable("compiler_so", CUDA["nvcc"])
             # use only a subset of the extra_postargs, which are 1-1
             # translated from the extra_compile_args in the Extension class
-            postargs = extra_postargs['nvcc']
+            postargs = extra_postargs["nvcc"]
         else:
-            postargs = extra_postargs['gcc']
+            postargs = extra_postargs["gcc"]
 
         super(obj, src, ext, cc_args, postargs, pp_opts)
         # Reset the default compiler_so, which we might have changed for cuda
@@ -89,13 +95,11 @@ def customize_compiler_for_nvcc(self):
     self._compile = _compile
 
 
-
 # Run the customize_compiler
 class custom_build_ext(build_ext):
     def build_extensions(self):
         customize_compiler_for_nvcc(self.compiler)
         build_ext.build_extensions(self)
-
 
 
 CUDA = locate_cuda()
@@ -106,36 +110,68 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
+ext2 = Extension(
+    "gpurenderer",
+    sources=[
+        "csrc/manager.cu",
+        "wrapper.pyx",
+    ],
+    library_dirs=[CUDA["lib64"]],
+    libraries=["cudart"],
+    language="c++",
+    runtime_library_dirs=[CUDA["lib64"]],
+    # This syntax is specific to this build system
+    # we're only going to use certain compiler args with nvcc
+    # and not with gcc the implementation of this trick is in
+    # customize_compiler()
+    extra_compile_args={
+        "gcc": [],
+        "nvcc": [
+            "-arch=sm_86",
+            "--ptxas-options=-v",
+            "-c",
+            "--compiler-options",
+            "'-fPIC'",
+        ],
+    },
+    include_dirs=[numpy_include, CUDA["include"], "./csrc"],
+)
 
-ext = Extension('gpuadder',
-        sources = ['src/manager.cu', 'wrapper.pyx'],
-        library_dirs = [CUDA['lib64']],
-        libraries = ['cudart'],
-        language = 'c++',
-        runtime_library_dirs = [CUDA['lib64']],
-        # This syntax is specific to this build system
-        # we're only going to use certain compiler args with nvcc
-        # and not with gcc the implementation of this trick is in
-        # customize_compiler()
-        extra_compile_args= {
-            'gcc': [],
-            'nvcc': [
-                '-arch=sm_86', '--ptxas-options=-v', '-c',
-                '--compiler-options', "'-fPIC'"
-                ]
-            },
-            include_dirs = [numpy_include, CUDA['include'], 'src']
-        )
+ext = Extension(
+    "raymarcher",
+    sources=[
+        "csrc/ray_marcher.cu",
+        "ray_marcher_wrapper.pyx",
+    ],
+    library_dirs=[CUDA["lib64"]],
+    libraries=["cudart"],
+    language="c++",
+    runtime_library_dirs=[CUDA["lib64"]],
+    # This syntax is specific to this build system
+    # we're only going to use certain compiler args with nvcc
+    # and not with gcc the implementation of this trick is in
+    # customize_compiler()
+    extra_compile_args={
+        "gcc": [],
+        "nvcc": [
+            "-arch=sm_86",
+            "--ptxas-options=-v",
+            "-c",
+            "--compiler-options",
+            "'-fPIC'",
+        ],
+    },
+    include_dirs=[numpy_include, CUDA["include"], "./csrc"],
+)
 
 
-
-setup(name = 'gpuadder',
-      version = '0.0.0',
-
-      ext_modules = [ext],
-
-      # Inject our custom trigger
-      cmdclass = {'build_ext': custom_build_ext},
-
-      # Since the package has c code, the egg cannot be zipped
-      zip_safe = False)
+setup(
+    name="cudapkg",
+    ext_package="cudapkg",
+    version="0.0.0",
+    ext_modules=[ext2],
+    # Inject our custom trigger
+    cmdclass={"build_ext": custom_build_ext},
+    # Since the package has c code, the egg cannot be zipped
+    zip_safe=False,
+)
